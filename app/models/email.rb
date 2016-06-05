beae5bc395f96498777feed503c9c79c156e8a17
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-05 19:45:53 +0100

    Wrote a test for calculating the open rate per email type

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,4 +2,6 @@ class Email < ActiveRecord::Base
   def self.count_total(event)
     group(:event).count[event]
   end
+
+  
 end
--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -11,5 +11,21 @@ describe Email, type: :model do
     it 'counts total number of emails sent' do
       expect(Email.count_total('send')).to eq 3
     end
+
+    it 'counts total number of emails opened' do
+      expect(Email.count_total('open')).to eq 2
+    end
+
+    it 'counts total number of emails clicked' do
+      expect(Email.count_total('click')).to eq 1
+    end
+  end
+
+  describe 'calculate_rate' do
+    it 'calculates the open rate per email type' do
+      2.times { FactoryGirl.create(:email, email_type: 'Order', event: 'open') }
+      3.times { FactoryGirl.create(:email, email_type: 'Order', event: 'send') }
+      expect(Email.calculate_rate('open', 'Order')).to eq '40%'
+    end
   end
 end
