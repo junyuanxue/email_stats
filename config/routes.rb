3d0a2235d1d83ff2845c02b47e695a3b3b9570a5
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-06 14:38:00 +0100

    Generated statistics controller and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :emails
+  root to: 'statistics#index'
+
+  resources :emails, :statistics
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
